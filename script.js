9a7ecf4f01a18989a9dd1be43e513444a53cf696
meri17283 <meririski17@gmail.com>
2025-03-19 00:09:01 +0800

    add change

--- script.js
+++ script.js
@@ -5,21 +5,4 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.style.transition = "opacity 3s";
         document.body.style.opacity = "1";
     }, 200);
-
-    // *** Efek Animasi Saat Scroll ***
-    const sections = document.querySelectorAll("section");
-
-    function checkScroll() {
-        sections.forEach(section => {
-            const sectionTop = section.getBoundingClientRect().top;
-            const windowHeight = window.innerHeight;
-
-            if (sectionTop < windowHeight - 100) {
-                section.classList.add("visible");
-            }
-        });
-    }
-
-    window.addEventListener("scroll", checkScroll);
-    checkScroll(); // Panggil sekali saat halaman dimuat
 });
