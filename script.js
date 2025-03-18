a9ced5422969a20aae3939f5c4536eac7838b80a
meri17283 <meririski17@gmail.com>
2025-03-19 00:12:44 +0800

    add change

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.style.transition = "opacity 3s";
         document.body.style.opacity = "1";
     }, 200);
+    document.getElementById("download-cv").addEventListener("click", function () {
+        const element = document.body; // Seluruh halaman akan dikonversi
+        html2pdf().from(element).save("CV_Merinda_Riski_Puspita_Sari.pdf");
+    });
 });
